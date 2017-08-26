450340719ae06e9dfc67ff83ddd833188bb22b47
Paulina Strychacz <paulina.strychacz@blstream.com>
2017-08-26 20:45:52 +0200

    Android Studio 2.3 project initialized. Created Game with basic kotlin tests.

---
+++ app/src/main/java/com/intive/hangman/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.intive.hangman.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.intive.hangman.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/com/intive/hangman/engine/GameTest.kt
@@ -0,0 +1,45 @@
+package com.intive.hangman.engine
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class GameTest {
+
+    val hangman = Game(
+            password = "microwave",
+            maxWrongAnswers = 20
+    )
+
+    @Test
+    fun `new game has 0 wrong answers`() {
+        assertEquals(0, hangman.wrongAnswers)
+    }
+
+    @Test
+    fun `new game has all letters dashed`() {
+        assertEquals("_________", hangman.dashedPassword)
+    }
+
+    @Test
+    fun `correct letter suggested`() {
+        val contains = hangman.suggestLetter('A')
+
+        assertTrue(contains)
+        assertEquals(0, hangman.wrongAnswers)
+    }
+
+    @Test
+    fun `wrong letter suggested`() {
+        val contains = hangman.suggestLetter('Z')
+
+        assertFalse(contains)
+        assertEquals(1, hangman.wrongAnswers)
+    }
+
+    @Test
+    fun `correct letter suggested should apear in dashedWord`() {
+        val contains = hangman.suggestLetter('A')
+        val dashed = hangman.dashedPassword
+        assertEquals ("______A__", dashed)
+    }
+}
